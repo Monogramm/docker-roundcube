1b30b535787327a7ea2aa12880fd79c2dfe8c0f0
mathieu.brunot <mb.mathieu.brunot@gmail.com>
2019-01-26 03:45:39 +0100

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+
+[uri_license]: http://www.gnu.org/licenses/gpl.html
+[uri_license_image]: https://img.shields.io/badge/License-GPL%20v3-blue.svg
+
+[![License: GPL v3][uri_license_image]][uri_license]
+[![Build Status](https://travis-ci.org/Monogramm/docker-roundcube.svg)](https://travis-ci.org/Monogramm/docker-roundcube)
+[![Docker Automated buid](https://img.shields.io/docker/build/monogramm/docker-roundcube.svg)](https://hub.docker.com/r/monogramm/docker-roundcube/)
+[![Docker Pulls](https://img.shields.io/docker/pulls/monogramm/docker-roundcube.svg)](https://hub.docker.com/r/monogramm/docker-roundcube/)
+
+# RoundCube custom Docker
+
+Custom Docker image for RoundCube.
+
+Provides a RoundCube with additional extensions for integrations of additional plugins.
+
+## What is RoundCube ?
+
+Roundcube Webmail is a browser-based multilingual IMAP client with an application-like user interface.
+
+> [roundcube.net](https://roundcube.net/)
+
+## Supported tags
+
+https://hub.docker.com/r/monogramm/docker-roundcube/
+
+-	`1.3.8-apache`, `1.3-apache`, `production-apache`, `1.3.8`, `1.3`, `production` (*images/1.3.8-apache/Dockerfile*)
+-	`1.3.8-fpm`, `1.3-fpm`, `production-fpm` (*images/1.3.8-fpm/Dockerfile*)
+-	`elastic`, `latest` (*images/elastic/Dockerfile*)
+
+## How to run this image ?
+
+See RoundCube base image documentation for details.
+
+> [RoundCube GitHub](https://github.com/roundcube/roundcubemail-docker)
+> [RoundCube DockerHub](https://hub.docker.com/r/roundcube/roundcubemail/)
+
+# Questions / Issues
+If you got any questions or problems using the image, please visit our [Github Repository](https://github.com/Monogramm/docker-roundcube) and write an issue.
---
+++ update.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+set -eo pipefail
+
+declare -A base=(
+	[apache]='debian'
+	[fpm]='debian'
+	[alpine]='alpine'
+)
+
+variants=(
+	apache
+	fpm
+)
+
+min_version='1.3'
+
+
+# version_greater_or_equal A B returns whether A >= B
+function version_greater_or_equal() {
+	[[ "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1" || "$1" == "$2" ]];
+}
+
+dockerRepo="monogramm/docker-roundcube"
+# Retrieve automatically the latest versions
+latests=(
+	'1.3.8'
+)
+specialLatests=(
+	elastic
+)
+
+# Remove existing images
+echo "reset docker images"
+find ./images -maxdepth 1 -type d -regextype sed -regex '\./images/[[:digit:]]\+\.[[:digit:]]\+' -exec rm -r '{}' \;
+
+echo "update docker images"
+travisEnv=
+for latest in "${latests[@]}"; do
+	version=$(echo "$latest" | cut -d. -f1-2)
+
+	if [ -d "$version" ]; then
+		continue
+	fi
+
+	# Only add versions >= "$min_version"
+	if version_greater_or_equal "$version" "$min_version"; then
+
+		for variant in "${variants[@]}"; do
+			echo "updating $latest [$version-$variant]"
+
+			# Create the version directory with a Dockerfile.
+			dir="images/$version-$variant"
+			mkdir -p "$dir"
+
+			template="Dockerfile-${base[$variant]}.template"
+			cp "$template" "$dir/Dockerfile"
+
+			# Replace the variables.
+			sed -ri -e '
+				s/%%VARIANT%%/-'"$variant"'/g;
+				s/%%VERSION%%/'"$latest"'/g;
+			' "$dir/Dockerfile"
+
+			travisEnv='\n    - VERSION='"$version"' VARIANT=-'"$variant$travisEnv"
+
+			if [[ $1 == 'build' ]]; then
+				tag="$version-$variant"
+				echo "Build Dockerfile for ${tag}"
+				docker build -t ${dockerRepo}:${tag} $dir
+			fi
+		done
+	fi
+
+done
+
+for latest in "${specialLatests[@]}"; do
+	version=$(echo "$latest" | cut -d. -f1-2)
+
+	if [ -d "$version" ]; then
+		continue
+	fi
+
+	# Only add versions >= "$min_version"
+	if version_greater_or_equal "$version" "$min_version"; then
+
+		echo "updating $latest [$version]"
+
+		# Create the version directory with a Dockerfile.
+		dir="images/$version"
+		mkdir -p "$dir"
+
+		template="Dockerfile-debian.template"
+		cp "$template" "$dir/Dockerfile"
+
+		# Replace the variables.
+		sed -ri -e '
+			s/%%VARIANT%%//g;
+			s/%%VERSION%%/'"$latest"'/g;
+		' "$dir/Dockerfile"
+
+		travisEnv='\n    - VERSION='"$version$travisEnv"
+
+		if [[ $1 == 'build' ]]; then
+			tag="$version"
+			echo "Build Dockerfile for ${tag}"
+			docker build -t ${dockerRepo}:${tag} $dir
+		fi
+	fi
+
+done
+
+# update .travis.yml
+travis="$(awk -v 'RS=\n\n' '$1 == "env:" && $2 == "#" && $3 == "Environments" { $0 = "env: # Environments'"$travisEnv"'" } { printf "%s%s", $0, RS }' .travis.yml)"
+echo "$travis" > .travis.yml
